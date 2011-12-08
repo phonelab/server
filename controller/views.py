773464f45eaea4abd01b06233d54e356db1caebb
lokesh mandvekar <lsm5@buffalo.edu>
2011-12-07 20:41:08 -0500

    first github upload

--- controller/views.py
+++ controller/views.py
@@ -1,9 +1,8 @@
-# Create your views here.
-
+import os
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_protect
 
 @csrf_protect
 def download_manifest(request, phone_id):
-  filename = ''.join(['/home/lsm5/manifests/', phone_id, '/manifest.xml'])
-  return HttpResponse(open(filename).read(), 'text/plain')
+  manifest_file = ''.join([os.getenv('HOME'), '/manifests/', phone_id, '/manifest.xml'])
+  return HttpResponse(open(manifest_file).read(), 'text/plain')
