5305b6a0f66e6c1c1bc478c1d867d07de286cd40
Micheal Benedict <micheal@visionmasterdesigns.com>
2012-02-20 13:11:36 -0500

    added awesome deployment script

--- config_production.py
+++ config_production.py
@@ -7,9 +7,9 @@ DATABASES = {
         'NAME'      : DATABASE_NAME,   # Or path to database file if using sqlite3.
         'USER'      : 'phonelab', # Not used with sqlite3.
         'PASSWORD'  : 'dbs3rv3r', # Not used with sqlite3.
-        'HOST'      : '', # Set to empty string for localhost. Not used with sqlite3.
+        'HOST'      : 'phonelab-backend-db.cg2akhwze6m8.us-east-1.rds.amazonaws.com', # Set to empty string for localhost. Not used with sqlite3.
         'PORT'      : '', # Set to empty string for default. Not used with sqlite3.
     }
 }
 
-DEBUG = False
+DEBUG = True
