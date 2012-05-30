599673c3aa939bcfd6089fe3104a7b20cb7204d0
Taeyeon Ki <taeyeon.ki@gmail.com>
2012-05-29 21:36:28 -0400

    changed the db of staging server

--- config_staging.py
+++ config_staging.py
@@ -1,13 +1,13 @@
-DATABASE_ENGINE = 'django.db.backends.sqlite3'
-DATABASE_NAME = 'server.sqlite3'
+DATABASE_ENGINE = 'django.db.backends.mysql'
+DATABASE_NAME = 'phonelab_staging'
 
 DATABASES = {
     'default': {
         'ENGINE'    : DATABASE_ENGINE, # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME'      : DATABASE_NAME,   # Or path to database file if using sqlite3.
-        'USER'      : '', # Not used with sqlite3.
-        'PASSWORD'  : '', # Not used with sqlite3.
-        'HOST'      : '', # Set to empty string for localhost. Not used with sqlite3.
+        'USER'      : 'phonelab', # Not used with sqlite3.
+        'PASSWORD'  : 'dbs3rv3r', # Not used with sqlite3.
+        'HOST'      : 'phonelab-backend-db.cg2akhwze6m8.us-east-1.rds.amazonaws.com', # Set to empty string for localhost. Not used with sqlite3.
         'PORT'      : '', # Set to empty string for default. Not used with sqlite3.
     }
 }
