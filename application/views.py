f57552cb49e11c8abb32d005a75322885fca7069
manojmc <manojmyl@buffalo.edu>
2012-07-11 15:12:51 -0400

    .

--- application/views.py
+++ application/views.py
@@ -69,7 +69,7 @@ def index(request):
   user = request.user
   userprofile = UserProfile.objects.get(user_id=user.id)
 
-  if userprofile.user_type == 'participant':
+  if userprofile.user_type == 'P':
     # query the database for all applications
     apps = Application.objects.all().order_by('-created')
 
