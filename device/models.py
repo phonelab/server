db0cd68197c3895c9e1327fc8719d750b2361fdd
Taeyeon Ki <taeyeon.ki@gmail.com>
2012-07-11 13:03:22 -0400

    changed device model

--- device/models.py
+++ device/models.py
@@ -19,7 +19,6 @@ class Device(models.Model):
     (u'E', u'ENABLED'),
     (u'D', u'DISABLED'),
   )
-  group           = models.ForeignKey(Group, blank=True, null=True)
   id              = models.CharField(max_length=15, null=False, primary_key=True)
 #  email           = models.CharField(max_length=30, null=False)
   reg_id          = models.CharField(max_length=300, null=False)
@@ -136,6 +135,7 @@ class DeviceProfile(models.Model):
   )
   dev           = models.ForeignKey(Device, unique=True)
   user          = models.ForeignKey(User, blank=True, null=True)
+  group         = models.ForeignKey(Group, blank=True, null=True)
   last_log      = models.DateTimeField(blank=True, null=True)
   phone_no      = models.CharField(max_length=13, blank=True, null=True)
   status        = models.CharField(max_length=1, choices=STATUS_CHOICES)
