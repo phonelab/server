f47519ea325b0f2a6a92646b7fcedb5017146c91
Taeyeon Ki <taeyeon.ki@gmail.com>
2012-07-19 11:02:19 -0400

    remove files

--- js/simple-confirm.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    $.fn.confirm = function() {
-        $(this).click(function(event) {
-            return confirm($(this).attr("title"));
-        });
-    };
-    
-    $('#confirm').confirm();
-})(jQuery);
