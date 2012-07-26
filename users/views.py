8d8a67b1b7c75a5b53b135774b339dfa22967645
Taeyeon Ki <taeyeon.ki@gmail.com>
2012-07-26 13:51:01 -0400

    added devicestatus

--- users/views.py
+++ users/views.py
@@ -220,7 +220,7 @@ def confirm(request, activation_key):
   userprofile.user.save()
   return render_to_response(
           'users/confirm.html', 
-          {'activated': True,}
+          {'activated': True}
          )
   
 
