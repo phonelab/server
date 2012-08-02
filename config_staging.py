7757b8b98a3805f41ddde840967984d9168853fa
Taeyeon Ki <taeyeon.ki@gmail.com>
2012-08-02 15:26:31 -0400

    change database from mysql to postgresql

--- config_staging.py
+++ config_staging.py
@@ -1,13 +1,18 @@
-DATABASE_ENGINE = 'django.db.backends.mysql'
-DATABASE_NAME = 'phonelab_staging'
+#DATABASE_ENGINE = 'django.db.backends.mysql'
+#DATABASE_NAME = 'phonelab_staging'
+DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'
+DATABASE_NAME = 'phonelab'
 
 DATABASES = {
     'default': {
         'ENGINE'    : DATABASE_ENGINE, # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME'      : DATABASE_NAME,   # Or path to database file if using sqlite3.
+#        'USER'      : 'phonelab', # Not used with sqlite3.
+#        'PASSWORD'  : 'dbs3rv3r', # Not used with sqlite3.
+#        'HOST'      : 'phonelab-backend-db.cg2akhwze6m8.us-east-1.rds.amazonaws.com', # Set to empty string for localhost. Not used with sqlite3.
         'USER'      : 'phonelab', # Not used with sqlite3.
-        'PASSWORD'  : 'dbs3rv3r', # Not used with sqlite3.
-        'HOST'      : 'phonelab-backend-db.cg2akhwze6m8.us-east-1.rds.amazonaws.com', # Set to empty string for localhost. Not used with sqlite3.
+        'PASSWORD'  : 'dbmanager', # Not used with sqlite3.
+        'HOST'      : '', # Set to empty string for localhost. Not used with sqlite3.
         'PORT'      : '', # Set to empty string for default. Not used with sqlite3.
     }
 }
